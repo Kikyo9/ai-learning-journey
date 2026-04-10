63244fa3221dac6c8f4ab55d3ade2f1e892be889
Kikyo9 <yan.zhang9921@outlook.com>
2026-04-10 21:22:41 +0800

    feat:增加年龄询问和年龄差值计算功能

--- hello_ai.py
+++ hello_ai.py
@@ -17,5 +17,20 @@ def main():
     print(f"\n你好，我是 {name}，你的 AI 学习伙伴。")
     print("我们的目标是：从零到一，成为 AI 工程师！")
 
+
+    ai_age = 3
+    try:
+        user_age = input("请问你今年多大啦？~")
+        user_age = int(user_age)
+        if user_age == ai_age:
+            print(f"哇塞~你和我一样大，都是{ai_age}岁")
+        elif user_age > ai_age:
+            print(f"哦~那你比我大，咱们差{user_age-ai_age}岁呢~")
+        else:
+            print(f"哦~那你比我小诶，我比你大{ai_age-user_age}岁呢~")
+    except:
+        print("要好好输入你的年龄哦，那明明是一个数字呢~")
+        
+
 if __name__ == "__main__":
     main()
